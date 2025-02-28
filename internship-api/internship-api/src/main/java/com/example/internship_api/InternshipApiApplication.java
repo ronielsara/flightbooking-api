f0c1a55de73d5ed71140a4539bde473e9157f287
roniel <sara.roniel@gmail.com>
2025-02-28 15:18:41 +0100

    Initial Commit

---
+++ internship-api/internship-api/src/main/java/com/example/internship_api/InternshipApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.internship_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipApiApplication.class, args);
+	}
+
+}
---
+++ internship-api/internship-api/src/test/java/com/example/internship_api/InternshipApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internship_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
